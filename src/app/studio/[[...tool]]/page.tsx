bd0e4518f48e0d7c13a3ab821af4ba7b37c6384b
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2026-04-06 23:44:47 +0100

    fix: negate navbar padding on Sanity Studio page

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -4,5 +4,9 @@ import { NextStudio } from "next-sanity/studio";
 import config from "../../../../sanity.config";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />;
+  return (
+    <div style={{ marginTop: "calc(var(--navbar-height) * -1)" }}>
+      <NextStudio config={config} />
+    </div>
+  );
 }
